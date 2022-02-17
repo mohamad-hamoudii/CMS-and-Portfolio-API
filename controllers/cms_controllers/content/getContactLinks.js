9c94e6ddbfc188f029c3235abf8387ca4b3285d3
Osama Hajj Ali <ossama.nae12@gmail.com>
2022-02-17 20:06:27 +0200

    update

--- controllers/cms_controllers/content/getContactLinks.js
+++ controllers/cms_controllers/content/getContactLinks.js
@@ -7,9 +7,9 @@ const getContactLinks = async (req, res) => {
 
     try {
         jwt.verify(token, secret);
-        if (!addContactLinks()) {
-            return res.json({ status: 'error', error: "Failed to create contacts" })
-        }
+        // if (!addContactLinks()) {
+        //     return res.json({ status: 'error', error: "Failed to create contacts" })
+        // }
 
         const contactLinks = await contactLinksModel.find();
 
@@ -19,38 +19,38 @@ const getContactLinks = async (req, res) => {
     }
 }
 
-const contacts = [
-    {
-        type: 'phone number',
-        contact: '+961 71 297 395',
-        isURL: false
-    },{
-        type: 'email',
-        contact: 'mohamadhamoudiphotography@gmail.com',
-        isURL: false
-    },{
-        type: 'Instagram',
-        contact: 'mohamadhamoudiphotography',
-        isURL: true,
-        URL: 'https://www.instagram.com/mohamadhamoudiphotography/'
-    },{
-        type: 'LinkedIn',
-        contact: 'mohamadhamoudiphotography',
-        isURL: true,
-        URL: 'https://www.instagram.com/mohamadhamoudiphotography/'
-    }
-]
-const addContactLinks = async () => {
-    try {
-        for (let i = 0; i < contacts.length;i++) {
-            await contactLinksModel.create(contacts[i])
-        }
+// const contacts = [
+//     {
+//         type: 'phone number',
+//         contact: '+961 71 297 395',
+//         isURL: false
+//     },{
+//         type: 'email',
+//         contact: 'mohamadhamoudiphotography@gmail.com',
+//         isURL: false
+//     },{
+//         type: 'Instagram',
+//         contact: 'mohamadhamoudiphotography',
+//         isURL: true,
+//         URL: 'https://www.instagram.com/mohamadhamoudiphotography/'
+//     },{
+//         type: 'LinkedIn',
+//         contact: 'mohamadhamoudiphotography',
+//         isURL: true,
+//         URL: 'https://www.instagram.com/mohamadhamoudiphotography/'
+//     }
+// ]
+// const addContactLinks = async () => {
+//     try {
+//         for (let i = 0; i < contacts.length;i++) {
+//             await contactLinksModel.create(contacts[i])
+//         }
 
-        return true;
-    }catch(error){
-        console.log(error);
-        return false;
-    }
-}
+//         return true;
+//     }catch(error){
+//         console.log(error);
+//         return false;
+//     }
+// }
 
 module.exports = getContactLinks;
